904c79ad127f9b5abd2ef66afa356990cf1a029e
Fkayensu <frederickayensuphd@gmail.com>
2024-03-12 01:09:47 -0400

    add high score tracking

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.querySelector('.guess').value = 23;
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = Number(document.querySelector('.score').textContent);
-let highScore;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -30,6 +30,9 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    highScore = Math.max(score, highScore);
+    document.querySelector('.highscore').textContent = highScore;
 
     // when guess is too high
   } else if (guess > secretNumber) {
@@ -57,7 +60,6 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(secretNumber);
   score = 20;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
